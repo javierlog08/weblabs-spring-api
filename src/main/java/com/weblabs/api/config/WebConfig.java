a4c1b2d9dff889bb515332eb59515c2adff5a0d8
Javier Girón <javierlog08@gmail.com>
2018-05-16 22:10:56 -0500

    Removed confused loginForm syntax on seurity

--- src/main/java/com/weblabs/api/config/WebConfig.java
+++ src/main/java/com/weblabs/api/config/WebConfig.java
@@ -19,9 +19,6 @@ class WebConfig implements WebMvcConfigurer {
 		.authorizeRequests()
 			.anyRequest().authenticated()
 			.and()
-		.formLogin()
-			.loginPage("/loginx")
-			.and()
 		.httpBasic();
 	}
 	
